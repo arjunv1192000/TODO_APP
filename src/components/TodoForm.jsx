d8cc56716589e785e21bae64132a3bc35e7c4a70
arjun <arjun.vmaniyara@gmail.com>
2023-05-18 20:51:10 +0530

    add local storage

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,19 +1,38 @@
-import React from 'react'
-import { useState } from 'react'
 
-export const TodoForm = ({addTodo}) => {
-    const [value,setvalue]= useState("")
-    const handleSubmit =e =>{
-        e.preventDefault();
+import React, { useState, useEffect, useRef } from 'react';
 
-        addTodo(value)
-        setvalue("")
+export const TodoForm = ({ addTodo }) => {
+  const [value, setValue] = useState('');
+  const inputRef = useRef(null);
 
+  useEffect(() => {
+    inputRef.current.focus();
+  }, []);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const trimmedValue = value.trim();
+
+    if (trimmedValue !== '') {
+      addTodo(trimmedValue);
+      setValue('');
     }
+  };
+
   return (
     <form className='TodoForm' onSubmit={handleSubmit}>
-        <input type="text" className='todo-input' value={value} placeholder='What is the task today ?  'onChange={(e)=> setvalue(e.target.value) }/>
-        <button type='submit' className='todo-btn'>Add</button>
+      <input
+        type='text'
+        className='todo-input'
+        value={value}
+        placeholder='What is the task today?'
+        onChange={(e) => setValue(e.target.value)}
+        ref={inputRef} // set the ref of the input element to inputRef
+      />
+      <button type='submit' className='todo-btn'>
+        Add
+      </button>
     </form>
-  )
-}
+  );
+};
--- src/components/Todowrapper.jsx
+++ src/components/Todowrapper.jsx
@@ -1,45 +1,105 @@
-import React from 'react'
-import { useState } from 'react'
+// import React from 'react'
+// import { useState } from 'react'
+// import { TodoForm } from './TodoForm'
+// import {v4 as uuidv4} from 'uuid';
+// import Todo from './Todo';
+// import { EditTodoForm } from './EditTodofrom';
+// uuidv4()
+
+// function Todowrapper() {
+//     const [todos,settodos]=useState([])
+//     const addTodo = todo =>{
+//         settodos([...todos,{id :uuidv4(),task :todo,
+//         completed :false,isEdited:false}])
+//         console.log(todos);
+
+//     }
+//     const togglecomplete =id =>{settodos(todos.map(todo =>todo.id === id ?{...todo, completed :! todo.completed }:todo))
+//     }
+
+//     const deletetodo = id =>{settodos(todos.filter(todo=> todo.id !== id))
+//     }
+//     const edittodo = id =>{settodos(todos.map(todo => todo.id === id ?{ ...todo,isEdited: !todo.isEdited}:todo))
+//     }
+
+//     const edittask=(task,id)=>{settodos(todos.map(todo=>todo.id === id ?{...todo,task,isEdited:!todo.isEdited}:todo))
+//     }
+//   return (
+//     <div className='TodoWrapper'>
+//         <h1>Todo App</h1>
+//         <TodoForm addTodo={addTodo}/>
+
+//         {todos.map((todo,index)=>(
+//             todo.isEdited ?(
+//                 <EditTodoForm edittodo={edittask} task={todo}/>
+//             ):(
+
+//                 <Todo task={todo} key={index} togglecomplete={togglecomplete} deletetodo={deletetodo} edittodo={edittodo} />
+
+//             )
+            
+//         ))}
+       
+//     </div>
+//   )
+// }
+
+// export default Todowrapper
+import React, { useState, useEffect } from 'react'
 import { TodoForm } from './TodoForm'
-import {v4 as uuidv4} from 'uuid';
+import { v4 as uuidv4 } from 'uuid';
 import Todo from './Todo';
 import { EditTodoForm } from './EditTodofrom';
-uuidv4()
 
 function Todowrapper() {
-    const [todos,settodos]=useState([])
-    const addTodo = todo =>{
-        settodos([...todos,{id :uuidv4(),task :todo,
-        completed :false,isEdited:false}])
-        console.log(todos);
+  const [todos, settodos] = useState(() => {
+    const storedTodos = localStorage.getItem('todos');
+    return storedTodos ? JSON.parse(storedTodos) : [];
+  });
 
-    }
-    const togglecomplete =id =>{settodos(todos.map(todo =>todo.id === id ?{...todo, completed :! todo.completed }:todo))
-    }
+  useEffect(() => {
+    localStorage.setItem('todos', JSON.stringify(todos));
+  }, [todos]);
 
-    const deletetodo = id =>{settodos(todos.filter(todo=> todo.id !== id))
-    }
-    const edittodo = id =>{settodos(todos.map(todo => todo.id === id ?{ ...todo,isEdited: !todo.isEdited}:todo))
-    }
+  const addTodo = todo => {
+    settodos([...todos, {
+      id: uuidv4(),
+      task: todo,
+      completed: false,
+      isEdited: false
+    }])
+  }
 
-    const edittask=(task,id)=>{settodos(todos.map(todo=>todo.id === id ?{...todo,task,isEdited:!todo.isEdited}:todo))
+  const togglecomplete = id => {
+    settodos(todos.map(todo => todo.id === id ? { ...todo, completed: !todo.completed } : todo))
+  }
+
+  const deletetodo = id => {
+    if (window.confirm('Are you sure you want to delete this todo?')) {
+      settodos(todos.filter(todo => todo.id !== id));
     }
+  }
+
+  const edittodo = id => {
+    settodos(todos.map(todo => todo.id === id ? { ...todo, isEdited: !todo.isEdited } : todo))
+  }
+
+  const edittask = (task, id) => {
+    settodos(todos.map(todo => todo.id === id ? { ...todo, task, isEdited: !todo.isEdited } : todo))
+  }
+
   return (
     <div className='TodoWrapper'>
-        <h1>Todo App</h1>
-        <TodoForm addTodo={addTodo}/>
+      <h1>Todo App</h1>
+      <TodoForm addTodo={addTodo} />
 
-        {todos.map((todo,index)=>(
-            todo.isEdited ?(
-                <EditTodoForm edittodo={edittask} task={todo}/>
-            ):(
-
-                <Todo task={todo} key={index} togglecomplete={togglecomplete} deletetodo={deletetodo} edittodo={edittodo} />
-
-            )
-            
-        ))}
-       
+      {todos.map((todo, index) => (
+        todo.isEdited ? (
+          <EditTodoForm edittodo={edittask} task={todo} key={todo.id} />
+        ) : (
+          <Todo task={todo} key={todo.id} togglecomplete={togglecomplete} deletetodo={deletetodo} edittodo={edittodo} />
+        )
+      ))}
     </div>
   )
 }
